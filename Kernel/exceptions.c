fedaf7947eae90a8728c773adaa114e07042e25f
ValentinaFernandezF <vfernandezflores@itba.edu.ar>
2024-11-05 10:19:33 -0300

    mejoras para exceptions

--- Kernel/exceptions.c
+++ Kernel/exceptions.c
@@ -1,19 +1,19 @@
-#include <registers.h>
+
 #define ZERO_EXCEPTION_ID 0
 
 static void zero_division();
 #include "exceptions.h"
 #include "videoDriver.h"
 #include "lib.h"
-extern uint64_t exception_regs[18];
+;extern uint64_t exception_regs[18];
 
-void exceptionDispatcher(int exception) {
+void exceptionDispatcher(int exception, uint64_t exceptionRegisters[18]) {
 	if (exception == ZERO_EXCEPTION_ID)
 		zero_division();
 	else{
 		invalidOperation();
 	}
-	printRegStatus(1); //&regBackup
+	printRegStatus(exceptionRegisters); //&regBackup
 	//vdNewline();
 	//vdPrint("Press keyboard to continue");
 	//getKeyPressed(); 
@@ -34,3 +34,11 @@ void invalidOperation(){
 	vdNewline();
 }
 
+static char * regs[] = {"RAX","RBX","RCX","RDX","RSI","RDI","RBP","RSP", "R8","R9","R10","R11","R12","R13","R14","R15", "RIP", "RFLAGS"};
+
+void printRegStatus(uint64_t exceptionRegisters[18]){
+	vdPrint("Exception registers:\n");
+    for (int i = 0; i<18; i++){
+        vdPrintRegister(regs[i], exceptionRegisters[i]);
+    }
+}
